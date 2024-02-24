7607f52598e75c4db51586b81fcc4284114fe249
nam232004 <93992069+nam232004@users.noreply.github.com>
2024-02-25 04:53:10 +0700

    Add files via upload
    birthdayForNNN

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ngô Ngọc Như';
+const giftUrl = '';
+const eventName = 'Happy Birthday';
+const titleCard = 'Gửi đến bạn';
+const contentCard = 'Vậy là đã đến tuổi đôi mươi, là dấu mốc quan trọng đầu tiên cho sự trưởng thành ở cả ngoại hình lẫn tâm hồn mỗi người. Có lẽ sau này bạn sẽ gặp nhiều thứ phải lo, nhiều điều phiền muộn, phải đối mặt với những khó khăn không mong muốn, nhưng mình tin rằng bạn sẽ luôn vượt qua, vì bạn mạnh mẽ, thông minh và có cá tính. Chúc bạn tuổi mới nhiều sức khỏe, con đường bạn đi luôn thuận lợi, gặp nhiều may mắn và gặt hái được nhiều thành công. Chúc bạn SINH NHẬT VUI VẺ !';
+
+const giftImage = '2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
